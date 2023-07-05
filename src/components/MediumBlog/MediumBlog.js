d8d82a24e0669f964f753ca59121a3248986f475
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-07-05 12:53:32 +0300

    exclude ua lang articles; prettier

--- src/components/MediumBlog/MediumBlog.js
+++ src/components/MediumBlog/MediumBlog.js
@@ -1,18 +1,20 @@
-import React, {useEffect, useState} from "react";
-import "./MediumBlog.css"
+import React, { useEffect, useState } from "react";
+import "./MediumBlog.css";
 
 const mediumRssFeed =
     "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fbrushfam";
 
 export const MediumBlog = () => {
-    const max_articles = 7;
+    const max_articles = 9;
     const [articles, setArticles] = useState();
 
     useEffect(() => {
         const loadArticles = async () => {
             fetch(mediumRssFeed, { headers: { Accept: "application/json" } })
                 .then((res) => res.json())
-                .then((data) => data.items.filter((item) => item.title.length > 0))
+                .then((data) =>
+                    data.items.filter((item) => item.title.length > 0)
+                )
                 .then((newArticles) => newArticles.slice(0, max_articles))
                 .then((articles) => setArticles(articles))
                 .catch((error) => console.log(error));
@@ -21,41 +23,48 @@ export const MediumBlog = () => {
         loadArticles();
     }, [max_articles]);
 
-    return(
+    return (
         <div className={"mediumContainer"}>
             <div className={"sectionTitle"}>
-                <img src={'/img/articles.svg'} style={{marginRight: 14}}/>
+                <img src={"/img/articles.svg"} style={{ marginRight: 14 }} />
                 <p>ARTICLES</p>
             </div>
 
             <div className={"mediumBlockContainer"}>
-            {articles
-                ? articles.map((item, i) => (
-                        item.title.slice(0,2) !== "Як" ?
-                            <div className={"mediumBlockWrapper"} key={i.toString()}>
-                                <a
-                                    href={item.link}
-                                    target="_blank"
-                                    title={item.title}
-                                    aria-label={item.title}
-                                >
-
-                                    <div className={"mediumBlock"}>
-                                        <img src={item.thumbnail} style={{padding: 0}} alt={item.title}/>
-                                        <div className={"descriptionBlock"}>
-                                            <p className={"articleTitle"}>
-                                                {item.title.replace("&amp;", "&")}
-                                            </p>
-                                            <p className={"articleDate"}>
-                                                {item.pubDate.slice(0,10)}
-                                            </p>
-                                        </div>
+                {articles?.map((item, i) =>
+                    item.title.match(/[\u0430-\u044f]/) ? (
+                        <></>
+                    ) : (
+                        <div
+                            className={"mediumBlockWrapper"}
+                            key={i.toString()}
+                        >
+                            <a
+                                href={item.link}
+                                target="_blank"
+                                title={item.title}
+                                aria-label={item.title}
+                            >
+                                <div className={"mediumBlock"}>
+                                    <img
+                                        src={item.thumbnail}
+                                        style={{ padding: 0 }}
+                                        alt={item.title}
+                                    />
+                                    <div className={"descriptionBlock"}>
+                                        <p className={"articleTitle"}>
+                                            {item.title.replace("&amp;", "&")}
+                                        </p>
+                                        <p className={"articleDate"}>
+                                            {item.pubDate.slice(0, 10)}
+                                        </p>
                                     </div>
-                                </a>
-                            </div> : <></>
-                ))
-                : "no article shown"}
+                                </div>
+                            </a>
+                        </div>
+                    )
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
